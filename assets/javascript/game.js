ca2883e91e51963644149cdc70987b0d85161a98
Nathan <nathanlserrano@gmail.com>
2018-10-27 22:30:19 -0700

    Finally Done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
-    // select random number to be shown at the start of the game (between 19 -120)
+    // select random Target number to be shown at the start of the game (between 19 -120)
     var randomNum = Math.floor(Math.random() * 102 + 19);
 
-    // adding random number to display on html
+    // adding random Target number to display on html
     $("#targetNum").text("Target: " + randomNum);
 
-    // set up random numbers for each jewel; 1-12
+    // set up random values for each jewel; 1-12
     var jewel1 = Math.floor(Math.random() * 11 + 1);
     var jewel2 = Math.floor(Math.random() * 11 + 1);
     var jewel3 = Math.floor(Math.random() * 11 + 1);
     var jewel4 = Math.floor(Math.random() * 11 + 1);
 
-    // set variables for counter, wins, losses
-    // update variables
+    // set global variables for wins, losses and userScore
+    // note: update these variables using jquery to manipulate html text
     var wins = 0;
     var losses = 0;
     var userScore = 0;
@@ -29,21 +29,21 @@ $(document).ready(function() {
         jewel4 = Math.floor(Math.random() * 11 + 1);
     }
 
-    // function for updating wins to the userScore
+    // function for updating wins... by using jquery to manipulate html text
     function updateWin() {
         wins++;
         $("#wins").text("Wins: " + wins);
         resettingStuff();
     }
     
-    // function for updating losses
+    // function for updating losses... by using jquery to manipulate html text
     function updateLoss() {
         losses++;
         $("#losses").text("Losses: " + losses);
         resettingStuff();
     }
 
-    // sets up click for jewels
+    // click function for each crystal!!!
     $("#red").on("click", function() {
         userScore = userScore + jewel1;
         $(".your_total").text("Your total score is: " + userScore);
@@ -87,11 +87,5 @@ $(document).ready(function() {
             updateLoss();
         }
     })
-
-
-
-
-
-
 
 })
